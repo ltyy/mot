e1c0fd42cee272c58a047d52ad9888299b087495
hongben <hongben@kingsoft.com>
2019-03-20 21:50:04 +0800

    ldap

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"time"
 
+	"github.com/ltyy/mot/ldap"
 	"github.com/shirou/gopsutil/host"
 )
 
@@ -17,5 +18,24 @@ func getUptime() uint64 {
 }
 
 func main() {
-	fmt.Println(getUptime())
+	//fmt.Println(getUptime())
+	var ad = &ldap.LDAP_CONFIG{
+		Addr:       "127.0.0.1:389",
+		BaseDn:     "ou=User,dc=wp,dc=cn",
+		BindDn:     "cn=admin,dc=wp,dc=cn",
+		BindPass:   "king",
+		AuthFilter: "(&(uid=%s))",
+		Attributes: []string{},
+		TLS:        false,
+		StartTLS:   false,
+	}
+
+	err := ad.Connect()
+	defer ad.Close()
+
+	success, err := ad.Auth("hongben", "abc123")
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(success)
 }
